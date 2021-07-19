bcb3e00a762ef80dd24ee4df8bda0f1f77fc9196
MangeshGadge <84099302+MangeshGadge@users.noreply.github.com>
2021-07-19 22:52:47 +0530

    Add files via upload

--- team_details.js
+++ team_details.js
@@ -96,8 +96,8 @@ function redirect(e) {
    const item = e.target;
   const id = item.parentElement.parentElement.getAttribute("player-id");
   // let l2 = l.join("") + "player_details.html" + "?playerId=" + id;
-  location.href.splice(l)
-  let l2 =  location.href+ "/player_details.html" + "?playerId=" + id;
+  const t = location.href.splice(l)
+  let l2 =  t + "/player_details.html" + "?playerId=" + id;
   location.href = l2;
 }
 
